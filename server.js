379bd28a8139e86f76c86b24867daa5b22ab6e65
Jacob Rothstein <github@jacobrothstein.com>
2010-08-28 00:04:04 -0700

    [ express ]

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8888, "0.0.0.0");
+
+var express = require("express");
+var app = express.createServer();
+app.configure((function() {
+  if (arguments.length > 0)
+    throw new Error("argument count mismatch: expected no arguments");
+  
+  return app.use(express.staticProvider((__dirname + "/public")));
+}));
+
+app.configure("development", (function() {
+  if (arguments.length > 0)
+    throw new Error("argument count mismatch: expected no arguments");
+  
+  return app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+}));
+
+app.configure("production", (function() {
+  if (arguments.length > 0)
+    throw new Error("argument count mismatch: expected no arguments");
+  
+  return app.use(express.errorHandler());
+}));
+
+app.get("/", (function(req, res) {
+  // req:required res:required
+  return res.send("Hello World");
+}));
+
+app.listen(8888);
+
+
---
+++ server.lisp
@@ -0,0 +1,20 @@
+(defvar express (require 'express))
+
+(defvar app (express.create-server))
+
+(app.configure (lambda ()
+	 (app.use (express.static-provider
+		   (concat **dirname "/public")))))
+
+(app.configure 'development
+       (lambda ()
+	 (app.use (express.error-handler
+		   (hash dump-exceptions true
+			 show-stack      true)))))
+
+(app.configure 'production (lambda ()
+    (app.use (express.error-handler))))
+
+(app.get "/" (lambda (req res) (res.send "Hello World")))
+
+(app.listen 8888)
